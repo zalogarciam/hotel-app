ff69b6d542b036b3ba02087b4b82df0b222eb150
zalogarciam <gegarciam95@gmail.com>
2022-12-12 16:11:26 -0500

    Updating list space

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const reservationModal = document.getElementById('exampleModal')
 const contactModal = document.getElementById('contactModal')
 
@@ -10,3 +12,29 @@ reservationModal.addEventListener('shown.bs.modal', () => {
 contactModal.addEventListener('shown.bs.modal', () => {
     myInput.focus()
   })
+
+
+const nav = document.querySelector('nav');
+const navMenuIcon = document.getElementById('nav-menu-icon');
+const listHeader = document.getElementById('list-header');
+const navCloseIcon = document.getElementById('icon-close');
+
+// const navMenuIcon = document.querySelector('#nav-menu-icon');
+console.log(navCloseIcon);
+console.log(navMenuIcon);
+
+document.addEventListener('scroll', () => {
+    nav.classList.toggle('nav--scroll', window.scrollY > 0);
+  });
+
+navCloseIcon.addEventListener('click', () => {
+    // listHeader.style.pointerEvents ='none';
+    // listHeader.style.opacity = 0;
+    listHeader.classList.remove('list--header-show');
+});
+
+navMenuIcon.addEventListener('click', () => {
+    // listHeader.style.pointerEvents ='auto';
+    // listHeader.style.opacity = 1;
+    listHeader.classList.add('list--header-show');
+});
